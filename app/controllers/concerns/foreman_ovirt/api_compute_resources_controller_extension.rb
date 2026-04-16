ab09cd157de50de988f255dcedcd34d02956f84f
Mansi Thakur <mansi.thakur.0502@gmail.com>
2026-04-16 06:43:11 +0000

    Fix oVirt datacenter name to UUID conversion in API

---
+++ app/controllers/concerns/foreman_ovirt/api_compute_resources_controller_extension.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module ForemanOvirt
+  module ApiComputeResourcesControllerExtension
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :convert_ovirt_datacenter_to_uuid, only: [:create, :update]
+    end
+
+    private
+
+    # Converts oVirt datacenter names to UUIDs before saving.
+    # This ensures API responses contain the UUID, not the datacenter name.
+    def convert_ovirt_datacenter_to_uuid
+      cr_params = params[:compute_resource]
+
+      # Check if this is an oVirt resource:
+      # - For create: check the provider param
+      # - For update: check the existing resource type
+      is_ovirt = if cr_params&.dig(:provider)&.downcase == 'ovirt'
+                   true
+                 elsif @compute_resource.is_a?(ForemanOvirt::Ovirt)
+                   true
+                 else
+                   false
+                 end
+
+      return unless is_ovirt
+
+      datacenter_param = cr_params&.dig(:datacenter)
+
+      return if datacenter_param.blank? || Foreman.is_uuid?(datacenter_param)
+
+      temp_cr = @compute_resource || ::ComputeResource.new_provider(cr_params.to_unsafe_hash.except(:datacenter))
+
+      if temp_cr.respond_to?(:get_datacenter_uuid)
+        temp_cr.test_connection
+        # Mutate the params object directly. When the core create/update method runs,
+        # it will read this updated UUID instead of the datacenter name string.
+        params[:compute_resource][:datacenter] = temp_cr.get_datacenter_uuid(datacenter_param)
+      end
+    end
+  end
+end
+
+
