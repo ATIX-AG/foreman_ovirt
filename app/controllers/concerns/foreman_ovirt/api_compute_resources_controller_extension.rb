fc88084dd1a78868de9345caf15f37ea4837e553
Mansi Thakur <mansi.thakur.0502@gmail.com>
2026-04-22 07:44:38 +0000

    Added requested comments to give context

--- app/controllers/concerns/foreman_ovirt/api_compute_resources_controller_extension.rb
+++ app/controllers/concerns/foreman_ovirt/api_compute_resources_controller_extension.rb
@@ -22,7 +22,10 @@ module ForemanOvirt
       datacenter_param = cr_params&.dig(:datacenter)
       return if datacenter_param.blank? || Foreman.is_uuid?(datacenter_param)
 
-      # Build temp CR with merged attributes for updates (to use new credentials if provided)
+      # Build temp CR credentials:
+      # - For UPDATE: @compute_resource exists. We merge new incoming params into existing attributes
+      #   so the temporary object has the URL/password needed to connect, even if not sent in this request.
+      # - For CREATE: @compute_resource is nil. We fallback to using just the incoming request params.
       merged_params = @compute_resource&.attributes&.merge(cr_params.to_unsafe_hash) || cr_params.to_unsafe_hash
       temp_cr = ::ComputeResource.new_provider(merged_params.except(:datacenter))
 
